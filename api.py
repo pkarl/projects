a3d44502158caf33bc38c5d1a71d490bf5ff112a
Pete Karl II <pete@bocoup.com>
2011-09-07 18:48:06 -0400

    added authorization flags to the resource handlers

--- api.py
+++ api.py
@@ -1,3 +1,4 @@
+from tastypie.authorization import Authorization
 from tastypie.resources import ModelResource
 from models import Company, Project, Client, Resource, Assignment
 
@@ -5,23 +6,28 @@ class CompanyResource(ModelResource):
     class Meta:
         queryset = Company.objects.all()
         resource_name = 'company'
+        authorization= Authorization()
 
 class ProjectResource(ModelResource):
     class Meta:
         queryset = Project.objects.all()
         resource_name = 'project'
+        authorization= Authorization()
 
 class ClientResource(ModelResource):
     class Meta:
         queryset = Client.objects.all()
         resource_name = 'client'
+        authorization= Authorization()
 
 class ResourceResource(ModelResource):
     class Meta:
         queryset = Resource.objects.all()
         resource_name = 'resource'
+        authorization= Authorization()
 
 class AssignmentResource(ModelResource):
     class Meta:
         queryset = Assignment.objects.all()
         resource_name = 'assignment'
+        authorization= Authorization()
--- urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls.defaults import patterns, include, url
-
-urlpatterns = patterns('',
-    (r'^polls/$', 'polls.views.index'),
-    (r'^polls/(?P<poll_id>\d+)/$', 'polls.views.detail'),
-    # (r'^polls/(?P<poll_id>\d+)/results/$', 'polls.views.results'),
-)
